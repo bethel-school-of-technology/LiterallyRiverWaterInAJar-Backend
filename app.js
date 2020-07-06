574c97bdce67d0a0d8c04af83d61d99652082f72
Braxton Harley <braxton.2012.harley@gmail.com>
2020-07-06 18:18:54 -0500

    changes to routes/migrations

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var models = require('./models')
+var models = require('./models');
+var passport = require('passport');  
+var session = require('express-session'); 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ secret: 'perilous journey' }));
+app.use(passport.initialize());  
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
