ab6a5a4f3a3b4d3959b56bdabd4f0f2afd8ff884
Braxton Harley <braxton.2012.harley@gmail.com>
2020-07-09 10:48:42 -0500

    added inventory, testimonials routes

---
+++ routes/testimonials.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+
+// render createTestimonial.js react screen
+router.get('', function (req, res, next) {
+
+});
+
+
+// create new testimonial
+router.post('', function (req, res, next) {
+   
+});
+
+
+// render editTestimonial.
+router.get("", function (req, res, next) {
+    
+});
+
+
+//  edit testimonial as user
+router.post("", function (req, res, next) {
+    
+});
+
+
+//  delete testimonial as user
+router.post("", function (req, res, next) {
+    
+});
+
+
+//  delete testimonial as admin
+router.post("", function (req, res, next) {
+    
+});
+
+
+module.exports = router;
